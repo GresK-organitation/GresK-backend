bebe3bf7698e78e4d386ea7d9c8540a251f2a3fa
Javier Castillo <javiercame98@gmail.com>
2026-03-23 19:55:40 +0100

    initial commit

---
+++ src/main/java/com/gresk/GreskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gresk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gresk/GreskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gresk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
