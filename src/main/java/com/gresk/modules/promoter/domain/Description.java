9c4266533392646f6ea6976ab48ad6e464e5dfc3
Javier Castillo <javiercame98@gmail.com>
2026-03-26 18:17:00 +0100

    feat: creating PromoterDescription as value Object

---
+++ src/main/java/com/gresk/modules/promoter/domain/Description.java
@@ -0,0 +1,52 @@
+package com.gresk.modules.promoter.domain;
+
+import java.util.Objects;
+
+public final class Description {
+
+    private static final int MIN_LENGTH = 2;
+    private static final int MAX_LENGTH = 600;
+
+    public final String value;
+
+    public Description (String value){
+        if (value == null || value.isBlank()) {
+            this.value = "No description";
+            return;
+        }
+
+        String cleanedValue = value.trim();
+
+        if (cleanedValue.length() < MIN_LENGTH || cleanedValue.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format("Description must have between %d and %d characters", MIN_LENGTH, MAX_LENGTH)
+            );
+        }
+        this.value = cleanedValue;
+    }
+
+    public boolean isEmpty() {
+        return value.isEmpty();
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Description that)) return false;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
