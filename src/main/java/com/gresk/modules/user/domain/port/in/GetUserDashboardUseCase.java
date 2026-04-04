c8f5831aecf7d0d2a687c6e49162d3fedeffd3fb
cristhian <cristhianchulca49@gmail.com>
2026-04-04 23:21:25 +0200

    refactor(user-application): Replace `UserId` with `UUID` in use case methods
    Update method signatures in `GetUserDashboardUseCase` and `UpdateUserUseCase` to accept `UUID` instead of `UserId`. Adjust implementations and related interfaces accordingly for improved flexibility and consistency.

--- src/main/java/com/gresk/modules/user/domain/port/in/GetUserDashboardUseCase.java
+++ src/main/java/com/gresk/modules/user/domain/port/in/GetUserDashboardUseCase.java
@@ -3,6 +3,8 @@ package com.gresk.modules.user.domain.port.in;
 import com.gresk.modules.user.application.dto.UserDashboardDTO;
 import com.gresk.modules.user.domain.model.UserId;
 
+import java.util.UUID;
+
 public interface GetUserDashboardUseCase {
-    UserDashboardDTO execute(UserId userId);
+    UserDashboardDTO execute(UUID userId);
 }
--- src/main/java/com/gresk/modules/user/domain/port/in/UpdateUserUseCase.java
+++ src/main/java/com/gresk/modules/user/domain/port/in/UpdateUserUseCase.java
@@ -3,6 +3,8 @@ package com.gresk.modules.user.domain.port.in;
 import com.gresk.modules.user.application.command.UpdateUserCommand;
 import com.gresk.modules.user.domain.model.User;
 
+import java.util.UUID;
+
 public interface UpdateUserUseCase {
-    User execute(UpdateUserCommand command);
+    User execute(UUID userId, UpdateUserCommand command);
 }
