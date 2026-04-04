dc8bfaf8d3efe0083fc46e8e902d94c10fbbef61
cristhian <cristhianchulca49@gmail.com>
2026-04-04 23:07:31 +0200

    refactor(user-domain): Remove unused imports in `UserRepositoryPort` interface

--- src/main/java/com/gresk/modules/user/domain/port/out/UserRepositoryPort.java
+++ src/main/java/com/gresk/modules/user/domain/port/out/UserRepositoryPort.java
@@ -2,9 +2,7 @@ package com.gresk.modules.user.domain.port.out;
 
 import com.gresk.modules.user.domain.model.User;
 import com.gresk.modules.user.domain.model.UserId;
-import com.gresk.shared.domain.valueobject.Email;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepositoryPort {
