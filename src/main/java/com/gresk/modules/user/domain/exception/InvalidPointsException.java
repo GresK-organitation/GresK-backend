5cb2806c47d7f6199c0ce802d1cd0e6e81f04a15
cristhian <cristhianchulca49@gmail.com>
2026-04-04 22:26:11 +0200

    feat(user-domain): Add custom exception for invalid loyalty points
    Replace `IllegalArgumentException` with `InvalidPointsException` in `User` model to improve clarity and consistency in error handling. Add `InvalidPointsException` class to encapsulate specific validation logic.

---
+++ src/main/java/com/gresk/modules/user/domain/exception/InvalidPointsException.java
@@ -0,0 +1,7 @@
+package com.gresk.modules.user.domain.exception;
+
+public class InvalidPointsException extends RuntimeException {
+    public InvalidPointsException(String message) {
+        super(message);
+    }
+}
