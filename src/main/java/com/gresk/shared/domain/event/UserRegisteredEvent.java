c02f5de41294fbcfb715d4a452a00ff29faef384
cristhian <cristhianchulca49@gmail.com>
2026-04-08 11:31:21 +0200

    feat(shared-domain): Add `UserRegisteredEvent` and extend `Role` enum with `PROMOTER_PENDING`
    Introduce `UserRegisteredEvent` to represent user registration events and enhance `Role` enum with a new value, `PROMOTER_PENDING`, for improved role management.

---
+++ src/main/java/com/gresk/shared/domain/event/UserRegisteredEvent.java
@@ -0,0 +1,13 @@
+package com.gresk.shared.domain.event;
+
+import java.util.Set;
+import java.util.UUID;
+
+public record UserRegisteredEvent(
+        UUID userId,
+        String email,
+        String name,
+        String description,
+        String city,
+        Set<String> musicGenres
+) {}
