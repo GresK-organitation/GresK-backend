f69232398e5afdc25090c111e7b092ca6a5debaa
cristhian <cristhianchulca49@gmail.com>
2026-04-09 10:39:30 +0200

    feat(command): Add avatar and logo support to user and promoter registration commands
    - Introduced `MultipartFile` field for user avatars in `RegisterUserAccountCommand`.
    - Added `MultipartFile` field for promoter logos in `RegisterPromoterAccountCommand`.

--- src/main/java/com/gresk/modules/identity/application/command/RegisterUserAccountCommand.java
+++ src/main/java/com/gresk/modules/identity/application/command/RegisterUserAccountCommand.java
@@ -2,8 +2,10 @@ package com.gresk.modules.identity.application.command;
 
 import com.gresk.shared.domain.Role;
 import lombok.Builder;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Set;
+
 @Builder
 public record RegisterUserAccountCommand(
         String email,
@@ -11,7 +13,8 @@ public record RegisterUserAccountCommand(
         String name,
         String description,
         String city,
-        Set<String> musicGenres
+        Set<String> musicGenres,
+        MultipartFile avatar
 ) {
     public Set<Role> roles() {
         return Set.of(Role.USER);
