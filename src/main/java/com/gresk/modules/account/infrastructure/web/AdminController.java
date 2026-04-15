03532fe49cfdb3ff7a2226fc64effdaffb04d6a8
cristhian <cristhianchulca49@gmail.com>
2026-04-15 09:56:43 +0200

    feat(account): add admin utilities for filtering users and promoters
    - Added `findUsersForAdmin` and `findPromotersForAdmin` methods with filtering by status and city.
    - Introduced role and status update methods in `AccountEntity` for improved entity management.
    - Enhanced `save` method in `JpaAccountRepositoryAdapter` to handle updates to existing accounts.

---
+++ src/main/java/com/gresk/modules/account/infrastructure/web/AdminController.java
@@ -0,0 +1,60 @@
+package com.gresk.modules.account.infrastructure.web;
+
+import com.gresk.modules.account.application.dto.AccountAdminSummary;
+import com.gresk.modules.account.application.usecase.*;
+import com.gresk.shared.domain.AccountStatus;
+import com.gresk.shared.domain.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@PreAuthorize("hasRole('ADMIN')")
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    private final ActivateAccountUseCase activateAccountUseCase;
+    private final ApprovePromoterUseCase approvePromoterUseCase;
+    private final SuspendAccountUseCase suspendAccountUseCase;
+    private final GetAccountsForAdminUseCase getAccountsUseCase;
+
+    @GetMapping("/promoters")
+    public ResponseEntity<List<AccountAdminSummary>> getPromoters(
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String status) {
+
+        var result = getAccountsUseCase.execute(Role.PROMOTER, city, status);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<AccountAdminSummary>> getUsers(
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String status) {
+
+        var result = getAccountsUseCase.execute(Role.USER, city, status);
+        return ResponseEntity.ok(result);
+    }
+
+    @PatchMapping("/promoter/{id}/active")
+    public ResponseEntity<Void> activeAccount(@PathVariable UUID id) {
+        activateAccountUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/account/{id}/suspend")
+    public ResponseEntity<Void> suspendAccount(@PathVariable UUID id) {
+        suspendAccountUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/account/{id}/approve")
+    public ResponseEntity<Void> approvePromoter(@PathVariable UUID id) {
+        approvePromoterUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }
+}
