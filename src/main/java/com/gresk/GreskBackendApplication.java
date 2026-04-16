f49a02242a47c1388399682fe9f8edacd62de415
cristhian <cristhianchulca49@gmail.com>
2026-04-16 08:31:16 +0200

    feat(config): enable caching and Feign clients in application startup
    - Added `@EnableCaching` annotation for caching support.
    - Introduced `@EnableFeignClients` to allow Feign client usage.

--- src/main/java/com/gresk/GreskBackendApplication.java
+++ src/main/java/com/gresk/GreskBackendApplication.java
@@ -2,8 +2,12 @@ package com.gresk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
+@EnableCaching // <--- AÑADE ESTO PARA QUE FUNCIONE EL CACHÉ
 public class GreskBackendApplication {
 
 	public static void main(String[] args) {
