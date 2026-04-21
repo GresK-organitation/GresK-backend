da8fee14e6931012126a7ddc036364751b03ab7d
Javier Castillo <javiercame98@gmail.com>
2026-04-21 23:00:43 +0200

    feat: fixing bugs

--- src/main/java/com/gresk/modules/user/infrastructure/config/SpotifyConfig.java
+++ src/main/java/com/gresk/modules/user/infrastructure/config/SpotifyConfig.java
@@ -1,4 +1,15 @@
 package com.gresk.modules.user.infrastructure.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spotify")
+@Getter
+@Setter
 public class SpotifyConfig {
+    private String clientId;
+    private String clientSecret;
 }
