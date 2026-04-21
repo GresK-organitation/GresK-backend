aea32d372602594749951661721762cc14143482
Javier Castillo <javiercame98@gmail.com>
2026-04-21 23:00:43 +0200

    feat: fixing bugs

--- src/main/java/com/gresk/GreskBackendApplication.java
+++ src/main/java/com/gresk/GreskBackendApplication.java
@@ -2,8 +2,10 @@ package com.gresk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.gresk.modules.user.infrastructure.adapters.external.spotify")
 public class GreskBackendApplication {
 
 	public static void main(String[] args) {
