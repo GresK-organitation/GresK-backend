e917a922a7d01baff21f7a871bdbcbc3f730fce4
cristhian <cristhianchulca49@gmail.com>
2026-05-18 21:45:10 +0200

    refactor: update AccountMapper and JpaAccountRepositoryAdapter for consistency and clarity

--- src/main/java/com/gresk/modules/account/infrastructure/persistence/JpaAccountAdapter.java
+++ src/main/java/com/gresk/modules/account/infrastructure/persistence/JpaAccountAdapter.java
@@ -3,30 +3,21 @@ package com.gresk.modules.account.infrastructure.persistence;
 import com.gresk.modules.account.domain.model.Account;
 import com.gresk.modules.account.domain.model.AccountId;
 import com.gresk.modules.account.domain.port.out.AccountRepositoryPort;
-import com.gresk.modules.promoter.domain.model.Promoter;
-import com.gresk.modules.promoter.infrastructure.persitence.PromoterMapper;
-import com.gresk.modules.user.domain.model.User;
-import com.gresk.modules.user.infrastructure.persistence.mapper.UserPersistenceMapper;
-import com.gresk.shared.domain.AccountStatus;
-import com.gresk.shared.domain.valueobject.City;
+import com.gresk.modules.account.infrastructure.persistence.jpa.AccountJpaRepository;
 import com.gresk.shared.domain.valueobject.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class JpaAccountRepositoryAdapter implements AccountRepositoryPort {
+public class JpaAccountAdapter implements AccountRepositoryPort {
 
     private final AccountJpaRepository jpaRepository;
     private final AccountMapper mapper;
-    private final PromoterMapper promoterMapper;
-    private final UserPersistenceMapper userMapper;
-
     @Override
     @Transactional
     public Account save(Account account) {
